815b84f56c4e18715c2ed64a76f349e475add803
eaadalen <eaadalen112@gmail.com>
2024-02-04 20:28:54 -0600

    1.3 updates

---
+++ Exercise-1.3/test.py
@@ -0,0 +1,5 @@
+destination = input("Where do you want to travel? ")
+if destination == "Minnesota" or destination == "New York" or destination == "California":
+    print("Enjoy your stay in " + destination + "!")
+else:
+    print("Oops, that destination is not currently available")
